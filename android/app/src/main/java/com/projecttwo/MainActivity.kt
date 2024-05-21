68a20e692e046ae4ccff5d1efc8f4aaff4f40785
Isaías Pérez <30501390+ispm19@users.noreply.github.com>
2024-05-21 00:40:03 -0400

    Update MainActivity.kt

--- android/app/src/main/java/com/projecttwo/MainActivity.kt
+++ android/app/src/main/java/com/projecttwo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.projecttwoispm19
+package com.projecttwo
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "projectTwoIspm19"
+  override fun getMainComponentName(): String = "projectTwo"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
